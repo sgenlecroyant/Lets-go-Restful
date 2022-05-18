627a52847e7d18c554f81a7195657aeb57ef720f
sgenlecroyant <sgenlecroyant@gmail.com>
2022-05-18 02:16:00 +0200

    set up the project

---
+++ src/main/java/com/sgenlecroyant/gorestful/LetsGoRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.gorestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsGoRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsGoRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgenlecroyant/gorestful/LetsGoRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.gorestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsGoRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
