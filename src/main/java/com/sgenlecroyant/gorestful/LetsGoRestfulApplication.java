f88091631bb502b7197dc1470642d27713dcc4c3
sgenlecroyant <sgenlecroyant@gmail.com>
2022-05-19 15:57:10 +0200

    implemented HATEOS

--- src/main/java/com/sgenlecroyant/gorestful/LetsGoRestfulApplication.java
+++ src/main/java/com/sgenlecroyant/gorestful/LetsGoRestfulApplication.java
@@ -1,14 +1,30 @@
 package com.sgenlecroyant.gorestful;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.context.annotation.Bean;
+
+import com.sgenlecroyant.gorestful.entity.User;
+import com.sgenlecroyant.gorestful.repository.UserRepository;
 
 @SpringBootApplication
 public class LetsGoRestfulApplication {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LetsGoRestfulApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		
+		User defaultUser = new User("James", "Bakana", "jamesbak@gmail.com", "password here");
+		return (args) -> {
+			 this.userRepository.save(defaultUser);
+		};
 	}
 }
--- src/main/java/com/sgenlecroyant/gorestful/controller/UserController.java
+++ src/main/java/com/sgenlecroyant/gorestful/controller/UserController.java
@@ -2,11 +2,12 @@ package com.sgenlecroyant.gorestful.controller;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,7 @@ import com.sgenlecroyant.gorestful.service.UserService;
 public class UserController {
 	
 	@Autowired
+	
 	private UserService userService;
 	
 	@PostMapping(value = "/users")
@@ -43,13 +45,29 @@ public class UserController {
 	}
 	
 	@GetMapping(value = "/users/{id}")
-	private ResponseEntity<User> fetchUserId(@PathVariable Integer id){
+	private ResponseEntity<EntityModel<User>> fetchUserId(@PathVariable Integer id) throws NoSuchMethodException, SecurityException{
+		User fetchedUser = this.userService.fetchUserById(id);
+		EntityModel<User> userEntityModel = EntityModel.of(fetchedUser);
+		
+		WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).fetchUsers());
+		userEntityModel.add(link.withRel("see-all-users"));
+		
+		
+		
+		return new ResponseEntity<EntityModel<User>>(
+				userEntityModel, HttpStatus.FOUND);
+	}
+	
+	@GetMapping(value = "/usersV2/{id}")
+	private ResponseEntity<User> fetchUserIdV2(@PathVariable Integer id) throws NoSuchMethodException, SecurityException{
+		User fetchedUser = this.userService.fetchUserById(id);
+		
 		return new ResponseEntity<User>(
-				this.userService.fetchUserById(id), HttpStatus.FOUND);
+				fetchedUser, HttpStatus.FOUND);
 	}
 	
 	@GetMapping(value = "/users")
-	private ResponseEntity<List<User>> fetchUsers(){
+	protected ResponseEntity<List<User>> fetchUsers(){
 		return new ResponseEntity<List<User>>(this.userService.fetchUsers(), HttpStatus.OK);
 	}
 	
