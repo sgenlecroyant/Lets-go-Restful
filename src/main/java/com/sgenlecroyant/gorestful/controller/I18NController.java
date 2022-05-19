5c359436324e6d5f46448ebb34937e8d2c700094
sgenlecroyant <sgenlecroyant@gmail.com>
2022-05-19 16:49:48 +0200

    Moved from getting the locale as a request argument to the LocaleContextHolder, no reading input since we have the default locale set

--- src/main/java/com/sgenlecroyant/gorestful/controller/I18NController.java
+++ src/main/java/com/sgenlecroyant/gorestful/controller/I18NController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,17 @@ public class I18NController {
 	@GetMapping(value = "/welcome")
 	public WelcomeMessage getWelcomeMessage(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
 		String message = this.messageSource.getMessage("welcome.message", null, locale);
-		
 		WelcomeMessage welcomeMessage = new WelcomeMessage(message);
 		return welcomeMessage;
 	}
+	
+
+	@GetMapping(value = "/welcome-v2")
+	public WelcomeMessage getWelcomeMessageV2() {
+		String message = this.messageSource.getMessage("welcome.message", null, LocaleContextHolder.getLocale());
+		WelcomeMessage welcomeMessage = new WelcomeMessage(message);
+		return welcomeMessage;
+	}
+	
 
 }
